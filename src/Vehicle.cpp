86b1b757901881208b40e376910119887639b32d
harhsithhn-glitch <harhsithhn@gmail.com>
2026-02-12 15:39:00 +0530

    Initial commit - Smart Parking Management System in C++

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -1,6 +1,8 @@
 #include "../include/Vehicle.h"
 
 Vehicle::Vehicle(const std::string& plateNumber, VehicleType vehicleType)
-    : plate(plateNumber), type(vehicleType), entryTime(std::time(nullptr))
+    : plate(plateNumber),
+      type(vehicleType),
+      entryTime(std::time(nullptr))
 {
 }
