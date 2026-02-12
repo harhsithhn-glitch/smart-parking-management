e3f70f0eb6b4b2bbc8779fb95db9caecd2f791bb
harhsithhn-glitch <harhsithhn@gmail.com>
2026-02-13 01:53:44 +0530

    Initial commit - Smart Parking Management System in C++

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include "../include/ParkingSystem.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ParkingSystem system(3, 5);
+
+    int choice;
+
+    while(true) {
+        cout << "\n===== ELITE SMART PARKING SYSTEM =====\n";
+        cout << "1. Show Status\n";
+        cout << "2. Park Vehicle\n";
+        cout << "3. Remove Vehicle\n";
+        cout << "4. Admin Panel\n";
+        cout << "5. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch(choice) {
+            case 1: system.showStatus(); break;
+            case 2: system.parkVehicle(); break;
+            case 3: system.removeVehicle(); break;
+            case 4: system.adminPanel(); break;
+            case 5: return 0;
+            default: cout << "Invalid choice!\n";
+        }
+    }
+
+    return 0;
+}
