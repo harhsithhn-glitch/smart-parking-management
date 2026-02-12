8447004d08dd3ff937845a7d199b8c4a54f3252c
harhsithhn-glitch <harhsithhn@gmail.com>
2026-02-13 02:07:24 +0530

    Initial commit - Smart Parking Management System in C++

--- src/ParkingSlot.cpp
+++ src/ParkingSlot.cpp
@@ -1,9 +1,29 @@
 #include "../include/ParkingSlot.h"
 
-ParkingSlot::ParkingSlot(int floorNumber, int slotNumber)
-    : floor(floorNumber),
-      number(slotNumber),
-      occupied(false),
-      vehicle(nullptr)
-{
+ParkingSlot::ParkingSlot(int f, int s)
+    : floor(f), slotNumber(s), available(true), vehicle("", VehicleType::CAR) {}
+
+bool ParkingSlot::isAvailable() const {
+    return available;
+}
+
+int ParkingSlot::getFloor() const {
+    return floor;
+}
+
+int ParkingSlot::getSlotNumber() const {
+    return slotNumber;
+}
+
+void ParkingSlot::parkVehicle(const Vehicle& v) {
+    vehicle = v;
+    available = false;
+}
+
+void ParkingSlot::removeVehicle() {
+    available = true;
+}
+
+const Vehicle& ParkingSlot::getVehicle() const {
+    return vehicle;
 }
